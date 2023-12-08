1c135fbd5ff6e7d6f319afc5b17d7a0727a2d215
Marcus Aspin <maspin@unilink.com>
2023-12-08 12:29:43 +0000

    PI-1720 Fix suggestion links not working since removing URL query parameter

--- src/utils/suggestions.test.ts
+++ src/utils/suggestions.test.ts
@@ -2,56 +2,68 @@ import { Request } from 'express'
 import getSuggestionLinks from './suggestions'
 
 describe('suggestions', () => {
-  it('should include the url', () => {
-    const suggestions = getSuggestionLinks(
-      {
-        suggestions: {
-          suggest: {
-            mispelled: [
+  const response = {
+    suggestions: {
+      suggest: {
+        mispelled: [
+          {
+            text: 'mispelled',
+            offset: 0,
+            length: 9,
+            options: [
               {
-                text: 'mispelled',
-                offset: 0,
-                length: 9,
-                options: [
-                  {
-                    text: 'misspelled',
-                    freq: 2,
-                    score: 2,
-                  },
-                  {
-                    text: 'mis-spelled',
-                    freq: 1,
-                    score: 1,
-                  },
-                ],
+                text: 'misspelled',
+                freq: 2,
+                score: 2,
               },
-            ],
-            qery: [
               {
-                text: 'qery',
-                offset: 10,
-                length: 4,
-                options: [
-                  {
-                    text: 'query',
-                    freq: 2,
-                    score: 2,
-                  },
-                ],
+                text: 'mis-spelled',
+                freq: 1,
+                score: 1,
               },
             ],
           },
-        },
+        ],
+        qery: [
+          {
+            text: 'qery',
+            offset: 10,
+            length: 4,
+            options: [
+              {
+                text: 'query',
+                freq: 2,
+                score: 2,
+              },
+            ],
+          },
+        ],
       },
-      { protocol: 'https', get: () => 'example.com', originalUrl: '/path?q=mispelled+qery' } as unknown as Request,
-    )
+    },
+  }
+
+  it('should include the new query', () => {
+    const suggestions = getSuggestionLinks(response, {
+      session: { probationSearch: { query: 'mispelled qery' } },
+    } as unknown as Request)
 
     expect(suggestions.length).toEqual(3)
     expect(suggestions[0]!.text).toEqual('misspelled')
-    expect(suggestions[0]!.url).toEqual('https://example.com/path?q=misspelled+qery')
+    expect(suggestions[0]!.newQuery).toEqual('misspelled qery')
     expect(suggestions[1]!.text).toEqual('query')
-    expect(suggestions[1]!.url).toEqual('https://example.com/path?q=mispelled+query')
+    expect(suggestions[1]!.newQuery).toEqual('mispelled query')
     expect(suggestions[2]!.text).toEqual('mis-spelled')
-    expect(suggestions[2]!.url).toEqual('https://example.com/path?q=mis-spelled+qery')
+    expect(suggestions[2]!.newQuery).toEqual('mis-spelled qery')
+  })
+
+  it('handles null session', () => {
+    const suggestions = getSuggestionLinks(response, { session: null } as unknown as Request)
+    expect(suggestions.length).toEqual(3)
+    expect(suggestions[0]!.text).toEqual('misspelled')
+    expect(suggestions[0]!.newQuery).toEqual('misspelled')
+    expect(suggestions[1]!.text).toEqual('query')
+    expect(suggestions[1]!.newQuery).toEqual('query')
+    expect(suggestions[2]!.text).toEqual('mis-spelled')
+    expect(suggestions[2]!.newQuery).toEqual('mis-spelled')
   })
 })
--- src/utils/suggestions.ts
+++ src/utils/suggestions.ts
@@ -1,27 +1,19 @@
 import { Request } from 'express'
-import { ProbationSearchResponse, Suggestion } from '../data/probationSearchClient'
-import { getAbsoluteUrl } from './url'
+import { ProbationSearchResponse } from '../data/probationSearchClient'
 
 export interface SuggestionLink {
   text: string
-  url: string
-}
-
-function getSuggestionUrl(req: Request, text: Suggestion): string {
-  const url = new URL(getAbsoluteUrl(req))
-  const oldQuery = url.searchParams.get('q') ?? ''
-  const newQuery = oldQuery.slice(0, text.offset) + text.text + oldQuery.slice(text.offset + text.length)
-  url.searchParams.set('q', newQuery)
-  return url.toString()
+  newQuery: string
 }
 
 export default function getSuggestionLinks(
   response: { suggestions?: ProbationSearchResponse['suggestions'] },
   req: Request,
 ): SuggestionLink[] {
+  const query = req.session?.probationSearch?.query ?? ''
   return Object.values(response?.suggestions?.suggest || {})
     .flatMap(suggestions => suggestions.flatMap(s => s.options.map(o => ({ ...s, ...o }))))
     .sort((a, b) => b.score - a.score || b.freq - a.freq)
     .slice(0, 3)
-    .map(s => ({ ...s, url: getSuggestionUrl(req, s) }))
+    .map(s => ({ ...s, newQuery: query.slice(0, s.offset) + s.text + query.slice(s.offset + s.length) }))
 }
