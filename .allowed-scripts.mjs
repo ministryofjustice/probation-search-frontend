71b5301f3f7b55c64ead9cd5f55ba2e6e9037e36
james.macbeth <james.macbeth@justice.gov.uk>
2026-03-30 15:27:35 +0100

    PI-3891 - Add cypress install step

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,5 +4,6 @@ export default configureAllowedScripts({
   allowlist: {
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/unrs-resolver@1.9.2': 'ALLOW',
+    'node_modules/cypress@15.12.0': 'ALLOW',
   },
 })
